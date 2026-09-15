4341a3fbd5b6464aa0ab17591dbb6c3d925f2085
vaani <seo@solutionstree.org>
2026-09-15 15:18:37 +0530

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -349,7 +349,12 @@ const Header = () => {
                         <div className="logo">
                             <Link to="/">
                                 <img
-                                    src="assets/img/logo/vero without bg.png"
+                                    src={
+                                    
+                                    isSticky
+                                        ? "assets/img/logo/vero without bg.png"
+                                        : "assets/img/home-1/whitelogo.png"
+                                    }
                                     alt="logo"
                                 />
                             </Link>
